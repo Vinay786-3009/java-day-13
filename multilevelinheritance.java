b024aebbb2c47c366687deb0a0f48625921451b9
B VINAY <vinay76b@gmail.com>
2024-12-14 11:05:20 +0530

    Add files via upload

---
+++ multilevelinheritance.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class a {
+    public void p1()
+    {
+        System.out.println("Hello");
+    }
+}
+class b extends a {
+    public void r(){
+        System.out.println("Hello Teachers");
+    }
+}
+class c extends b {
+    public void r1(){
+        System.out.println("Hello Professors");
+    }
+}
+public class multilevelinheritance
+{
+    public static void main(String args[])
+    {
+        c f=new c();
+        f.p1();
+        f.r();
+        f.r1();
+    }
+}
