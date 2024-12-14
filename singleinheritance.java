f506b7f5ff8de50bca8bfe5f57c5a5d3195e143e
B VINAY <vinay76b@gmail.com>
2024-12-14 11:03:57 +0530

    Add files via upload

---
+++ singleinheritance.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class a {
+    public void p1()
+    {
+        System.out.println("Hello");
+    }
+}
+class b extends a {
+    public void r(){
+        System.out.println("Hello Teachers");
+    }
+}
+public class singleinheritance
+{
+    public static void main(String args[])
+    {
+        a h=new a();
+        h.p1();
+        b y=new b();
+        y.p1();
+        y.r();
+    }
+}
