93f3d6f5ba4f3c53fe8a6000dc98b21cabd63a57
B VINAY <vinay76b@gmail.com>
2024-12-14 11:03:42 +0530

    Add files via upload

---
+++ superkeyword.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class a {
+    public void r()
+    {
+        System.out.println("Hello");
+    }
+}
+class b extends a {
+    public void r1(){
+        super.r();
+        System.out.println("Hello Teachers");
+    }
+}
+public class superkeyword
+{
+    public static void main(String args[])
+    {
+        b n=new b();
+        n.r1();
+    }
+}
